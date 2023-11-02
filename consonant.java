a6f3227912c40d5842ca0eb43de23005c8f8990d
AarthiPuli5 <aarthipuli@gmail.com>
2023-11-02 13:10:31 +0530

    first commit

---
+++ consonant.java
@@ -0,0 +1,12 @@
+class Consonant
+{
+   public static void main(String[] args)
+     {
+       char letter='e';
+       if(letter>'a'&&letter<='z'&&letter!='e'&&letter!='i'&&letter!='o'&&letter!='u')
+          System.out.println("consonant");
+       else
+          System.out.println("vowel");
+
+      }
+}
